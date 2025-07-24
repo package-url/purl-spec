9fb41cc4bff331b5a8ba611aeff5e54fbc077622
Abul Hasan Sheik Madhar Ali <abulhasan2k4@gmail.com>
2025-07-24 15:15:11 +0100

    Added the purl specs and types md files to the website

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,148 +1,17 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  mySidebar: [
+    {
+      type: 'doc',
+      id: 'PURL_SPECIFICATION', // must match the file's frontmatter `id` or filename (see note below)
+      label: 'PURL Specification',
+    },
+    {
+      type: 'doc',
+      id: 'PURL_TYPES', // must match the file's frontmatter `id` or filename (see note below)
+      label: 'PURL Types',
+    },
+  ],
+};
 
-        /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
-        const sidebars = {
-        mySidebar : [
- {
-  "type": "category",
-  "label": "Some_purl_examples",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Some_purl_examples",
-    "id": "Some_purl_examples/Some_purl_examples"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Known_purl_types",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Known_purl_types",
-    "id": "Known_purl_types/Known_purl_types"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "How_to_parse_a_purl_string_in_its_components",
-  "items": [
-   {
-    "type": "doc",
-    "label": "How_to_parse_a_purl_string_in_its_components",
-    "id": "How_to_parse_a_purl_string_in_its_components/How_to_parse_a_purl_string_in_its_components"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "A_purl_is_a_URL",
-  "items": [
-   {
-    "type": "doc",
-    "label": "A_purl_is_a_URL",
-    "id": "A_purl_is_a_URL/A_purl_is_a_URL"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "License",
-  "items": [
-   {
-    "type": "doc",
-    "label": "License",
-    "id": "License/License"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "purl_separators",
-  "items": [
-   {
-    "type": "doc",
-    "label": "purl_separators",
-    "id": "purl_separators/purl_separators"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Tests",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Tests",
-    "id": "Tests/Tests"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Known_qualifiers_keyvalue_pairs",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Known_qualifiers_keyvalue_pairs",
-    "id": "Known_qualifiers_keyvalue_pairs/Known_qualifiers_keyvalue_pairs"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Package_URL_specification_v10X",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Package_URL_specification_v10X",
-    "id": "Package_URL_specification_v10X/Package_URL_specification_v10X"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "How_to_build_purl_string_from_its_components",
-  "items": [
-   {
-    "type": "doc",
-    "label": "How_to_build_purl_string_from_its_components",
-    "id": "How_to_build_purl_string_from_its_components/How_to_build_purl_string_from_its_components"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Permitted_characters",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Permitted_characters",
-    "id": "Permitted_characters/Permitted_characters"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Character_encoding",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Character_encoding",
-    "id": "Character_encoding/Character_encoding"
-   }
-  ]
- },
- {
-  "type": "category",
-  "label": "Rules_for_each_purl_component",
-  "items": [
-   {
-    "type": "doc",
-    "label": "Rules_for_each_purl_component",
-    "id": "Rules_for_each_purl_component/Rules_for_each_purl_component"
-   }
-  ]
- }
-]}; export default sidebars;
+export default sidebars;
