16c6369e9a2c720ed72020f4f682130bfc2cc906
Abul Hasan Sheik Madhar Ali <abulhasan2k4@gmail.com>
2025-07-24 15:56:34 +0100

    setup frontend structure

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/PURL_SPECIFICATION">
+            View PURL Specification 📘
           </Link>
         </div>
       </div>
